4b9752b3a10067951f424ff60cbd24477d6c6986
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-27 14:41:20 +0200

    create db schema

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ app.get("/", (_req, res) => {
 	res.send("Hello World!")
 })
 
+app.post("/create-user", (req, res) => {
+	console.log(req.body)
+	res.send("User created")
+})
+
 app.listen(3000, () => {
 	console.log("Server running on port 3000")
 })
